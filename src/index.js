cb14db29266bd70ccd226cac69957c21a80fd916
AlekEngineer <calessin@gmail.com>
2018-09-30 01:02:16 +0300

    100 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let counter = 0;
+      i = 5;
+  while (number/i >= 1) {
+    counter += Math.floor(number/i);
+    i *= 5;
+  }
+return counter;
 }
